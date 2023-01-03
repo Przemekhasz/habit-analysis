f705ec682df67bcc6e1d3b205cb73e4e46762d72
Przemysław Tarapacki <116648508+Przemekhasz8@users.noreply.github.com>
2023-01-03 23:53:48 +0100

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
+import { logger } from './common/middleware/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(CatsController);
+  }
+}
